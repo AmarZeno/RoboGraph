5155a5566df1c11dc878d8a7e31f520deee42237
Zeno Saviour <zeno.saviour@utah.edu>
2018-03-11 12:21:52 -0600

    Added debugging files

---
+++ RoboGraph/Assets/Scripts/Debugging.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Debugging : MonoBehaviour {
+
+    public TextAsset DiagnosticFile;
+
+    void Start()
+    {
+        CleanDebuggingFiles();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+       // WriteString(DiagnosticFile, speedData);
+    }
+
+    private void CleanDebuggingFiles()
+    {
+        WriteString(DiagnosticFile, "");
+    }
+
+    [MenuItem("Tools/Write file")]
+    static void WriteString(TextAsset DiagnosticFile, string stringToWrite)
+    {
+        string path = AssetDatabase.GetAssetPath(DiagnosticFile);
+
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(stringToWrite);
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(path);
+      //  TextAsset asset = Resources.Load(path);
+
+        //Print the text from the file
+       // Debug.Log(asset.text);
+    }
+
+    [MenuItem("Tools/Read file")]
+    static void ReadString(TextAsset DiagnosticFile)
+    {
+        string path = AssetDatabase.GetAssetPath(DiagnosticFile);
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+    }
+}
