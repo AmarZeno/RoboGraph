d6fda5a68cbdb58cc88ff557c77c3fd7f1495431
Zeno Saviour <zeno.saviour@utah.edu>
2018-03-12 00:40:36 -0600

    Implemented Dijkstraus

--- RoboGraph/Assets/Scripts/RGSearchAlgorithms.cs
+++ RoboGraph/Assets/Scripts/RGSearchAlgorithms.cs
@@ -10,7 +10,7 @@ namespace RGGraphCore
 
         public static void BreadthFirstSearch<T>(RGGraph<T> graph, RGVertex<T> sourceVertex)
         {
-            foreach(RGVertex<T> vertex in graph.Vertices)
+            foreach (RGVertex<T> vertex in graph.Vertices)
             {
                 vertex.Parent = null;
                 vertex.Distance = 0;
@@ -20,12 +20,12 @@ namespace RGGraphCore
             Queue<RGVertex<T>> queue = new Queue<RGVertex<T>>();
             queue.Enqueue(sourceVertex);
 
-            while(queue.Count > 0)
+            while (queue.Count > 0)
             {
                 RGVertex<T> vertex = queue.Dequeue();
-                foreach(RGVertex<T> neighbour in graph.GetAdjacentVertices(vertex))
+                foreach (RGVertex<T> neighbour in graph.GetAdjacentVertices(vertex))
                 {
-                    if(!neighbour.Visited)
+                    if (!neighbour.Visited)
                     {
                         neighbour.Parent = vertex;
                         neighbour.Distance = vertex.Distance + graph.GetEdgeWeight(vertex, neighbour);
@@ -39,14 +39,13 @@ namespace RGGraphCore
             }
         }
 
-
         public static List<RGVertex<T>> BreadthFirstSearchWithGoal<T>(RGGraph<T> graph, RGVertex<T> sourceVertex, RGVertex<T> goalVertex)
         {
             if (sourceVertex.Equals(goalVertex))
             {
                 return new List<RGVertex<T>> { sourceVertex };
             }
-            foreach(RGVertex<T> vertex in graph.Vertices)
+            foreach (RGVertex<T> vertex in graph.Vertices)
             {
                 vertex.Parent = null;
                 vertex.Distance = 0;
@@ -56,19 +55,19 @@ namespace RGGraphCore
             Queue<RGVertex<T>> queue = new Queue<RGVertex<T>>();
             queue.Enqueue(sourceVertex);
 
-            while(queue.Count > 0)
+            while (queue.Count > 0)
             {
                 RGVertex<T> vertex = queue.Dequeue();
 
-                foreach(RGVertex<T> neighbour in graph.GetAdjacentVertices(vertex))
+                foreach (RGVertex<T> neighbour in graph.GetAdjacentVertices(vertex))
                 {
-                    if(!neighbour.Visited)
+                    if (!neighbour.Visited)
                     {
                         neighbour.Parent = vertex;
                         neighbour.Distance = vertex.Distance + graph.GetEdgeWeight(vertex, neighbour);
                         neighbour.Visited = true;
 
-                        if(neighbour.Equals(goalVertex))
+                        if (neighbour.Equals(goalVertex))
                         {
                             return GetPathToSource<T>(neighbour);
                         }
@@ -88,7 +87,7 @@ namespace RGGraphCore
             List<RGVertex<T>> path = new List<RGVertex<T>>();
             RGVertex<T> next = from;
 
-            while(next != null)
+            while (next != null)
             {
                 path.Add(next);
                 next = next.Parent;
@@ -97,14 +96,11 @@ namespace RGGraphCore
             return path;
         }
 
-
-
-
         // DFS BEGINS
 
         public static void DepthFirstSearch<T>(RGGraph<T> graph, RGVertex<T> sourceVertex, bool reverseNeighbours = false)
         {
-            foreach(RGVertex<T> vertex in graph.Vertices)
+            foreach (RGVertex<T> vertex in graph.Vertices)
             {
                 vertex.Parent = null;
                 vertex.Distance = 0;
@@ -115,18 +111,18 @@ namespace RGGraphCore
             Stack<RGVertex<T>> stack = new Stack<RGVertex<T>>();
             stack.Push(sourceVertex);
 
-            while(stack.Count > 0)
+            while (stack.Count > 0)
             {
                 RGVertex<T> vertex = stack.Pop();
 
                 List<RGVertex<T>> neighbours = graph.GetAdjacentVertices(vertex);
-                if(reverseNeighbours)
+                if (reverseNeighbours)
                 {
                     neighbours.Reverse();
                 }
-                foreach(RGVertex<T> neighbour in neighbours)
+                foreach (RGVertex<T> neighbour in neighbours)
                 {
-                    if(!neighbour.Visited)
+                    if (!neighbour.Visited)
                     {
                         neighbour.Parent = vertex;
                         neighbour.Distance = vertex.Distance + graph.GetEdgeWeight(vertex, neighbour);
@@ -142,12 +138,12 @@ namespace RGGraphCore
 
         public static List<RGVertex<T>> DepthFirstSearchWithGoal<T>(RGGraph<T> graph, RGVertex<T> sourceVertex, RGVertex<T> goalVertex, bool reverseNeighbours = false)
         {
-            if(sourceVertex.Equals(goalVertex))
+            if (sourceVertex.Equals(goalVertex))
             {
                 return new List<RGVertex<T>> { sourceVertex };
             }
 
-            foreach(RGVertex<T> vertex in graph.Vertices)
+            foreach (RGVertex<T> vertex in graph.Vertices)
             {
                 vertex.Parent = null;
                 vertex.Distance = 0;
@@ -175,7 +171,7 @@ namespace RGGraphCore
                         neighbour.Distance = vertex.Distance + graph.GetEdgeWeight(vertex, neighbour);
                         neighbour.Visited = true;
 
-                        if(neighbour.Equals(goalVertex))
+                        if (neighbour.Equals(goalVertex))
                         {
                             return GetPathToSource<T>(neighbour);
                         }
@@ -190,6 +186,80 @@ namespace RGGraphCore
             return null;
         }
 
+        public static void Dijkstra<T>(RGGraph<T> graph, RGVertex<T> source)
+        {
+            List<RGVertex<T>> unfinishedVertices = new List<RGVertex<T>>();
+            foreach (RGVertex<T> vertex in graph.Vertices)
+            {
+                vertex.Distance = int.MaxValue;
+                vertex.Parent = null;
+                unfinishedVertices.Add(vertex);
+            }
+            source.Distance = 0;
+            while (unfinishedVertices.Count > 0)
+            {
+                RGVertex<T> vertex = GetClosestVertex(unfinishedVertices);
+                unfinishedVertices.Remove(vertex);
+                foreach (RGVertex<T> adjVertex in graph.GetAdjacentVertices(vertex))
+                {
+                    if (adjVertex.Distance > vertex.Distance + graph.GetEdgeWeight(vertex, adjVertex))
+                    {
+                        adjVertex.Distance = vertex.Distance + graph.GetEdgeWeight(vertex, adjVertex);
+                        adjVertex.Parent = vertex;
+                    }
+                }
+            }
+        }
+
+        public static List<RGVertex<T>> DijkstraWithGoal<T>(RGGraph<T> graph, RGVertex<T> source, RGVertex<T> goal)
+        {
+            if(source.Equals(goal))
+            {
+                return new List<RGVertex<T>> { source };
+            }
+
+            List<RGVertex<T>> unfinishedVertices = new List<RGVertex<T>>();
+            foreach (RGVertex<T> vertex in graph.Vertices)
+            {
+                vertex.Distance = int.MaxValue;
+                vertex.Parent = null;
+                unfinishedVertices.Add(vertex);
+            }
+            source.Distance = 0;
+            while (unfinishedVertices.Count > 0)
+            {
+                RGVertex<T> vertex = GetClosestVertex(unfinishedVertices);
+                unfinishedVertices.Remove(vertex);
+                if(vertex.Equals(goal))
+                {
+                    return GetPathToSource(vertex);
+                }
+                foreach (RGVertex<T> adjVertex in graph.GetAdjacentVertices(vertex))
+                {
+                    if (adjVertex.Distance > vertex.Distance + graph.GetEdgeWeight(vertex, adjVertex))
+                    {
+                        adjVertex.Distance = vertex.Distance + graph.GetEdgeWeight(vertex, adjVertex);
+                        adjVertex.Parent = vertex;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static RGVertex<T> GetClosestVertex<T>(List<RGVertex<T>> list)
+        {
+            RGVertex<T> candidate = list[0];
+            foreach (RGVertex<T> vertex in list)
+            {
+                if (vertex.Distance < candidate.Distance)
+                {
+                    // I can use binomial heap if required
+                    candidate = vertex;
+                }
+            }
+
+            return candidate;
+        }
     }
 }
 
